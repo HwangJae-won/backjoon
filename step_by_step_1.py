56a410953819a5c92e5f3e6ce244743432f9d2fc
HwangJae-won <79994991+HwangJae-won@users.noreply.github.com>
2022-07-05 18:00:53 +0900

    Initial commit

---
+++ step_by_step_1.py
@@ -0,0 +1,60 @@
+#2557
+print("Hello world!")
+
+#10718
+print("강한친구 대한육군") ; print("강한친구 대한육군")
+
+#10171
+print("\    /\ ") ; print(" )  ( ')") ; print("(  /  )") ;print(" \(__)|")
+
+#10172
+print("|\_/|") ; print("|q p|   /}"); print('( 0 )"""\ ') ; print('|"^"`    |') ; print(r"||_/=\\__|")
+
+#1000
+a,b = map(int, input().split())
+print(a+b)
+
+#1001
+a,b = map(int, input().split())
+print(a-b)
+
+#10998
+a,b = map(int, input().split())
+print(a*b)
+
+#1008
+a,b= map(int, input().split())
+print(a/b)
+
+#10869
+A, B = map(int, input().split())
+print(A+B) ; print(A-B) ; print(A*B) ; print(A//B) ;print(A%B)
+
+#10926
+id = str(input())
+print(id+'??!')
+
+#18108
+y=int(input())
+print(y-543)
+
+#10430
+A , B, C = map(int, input().split())
+print((A+B) % C) ; print(((A%C) + (B%C))%C) ; print((A*B)%C) ; print((A%C * B%C) % C)
+
+#2588
+n1 = int(input()) ; n2 = input()
+n3= n1 * int(n2[2])
+n4= n1 * int(n2[1])
+n5= n1 * int(n2[0])
+n6 = n3 + n4*10 + n5 *100
+print(n3) ; print(n4) ;print(n5) ;print(n6)
+
+# 25083
+pic= """         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |"""
+print(pic)
