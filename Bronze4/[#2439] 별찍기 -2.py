268d7033765ad5b95639588df50148569ed082b1
Jaewon Hwang <79994991+HwangJae-won@users.noreply.github.com>
2022-07-15 18:40:13 +0900

    Add files via upload

---
+++ Bronze4/[#2439] 별찍기 -2.py
@@ -0,0 +1,8 @@
+#2439 별찍기 -2
+num = int(input())
+for i in range(1,num+1):
+    print(" " * (num-i) + '*' * i)
+
+
+
+
---
+++ Bronze4/[#2440]별찍기 -3.py
@@ -0,0 +1,8 @@
+#2440 별찍기 -3
+num = int(input())
+for i in range(0,num+1):
+    print('*' * (num - i))
+
+
+
+
