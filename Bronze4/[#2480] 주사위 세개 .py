78fc459eb7f30e59174fc22ac3cf2a090295f7f9
Jaewon Hwang <79994991+HwangJae-won@users.noreply.github.com>
2022-07-16 12:04:39 +0900

    Add files via upload

---
+++ Bronze4/[#2480] 주사위 세개 .py
@@ -0,0 +1,10 @@
+#2480 주사위 세 개
+n1, n2, n3 = map(int, input().split())
+if n1 == n2 == n3:
+    print(10000 + n1*1000)
+elif n1 == n2 or n2 == n3 :
+    print(1000 + n2 * 100)
+elif n1 == n3:
+    print(1000+ n1 *100 )
+else:
+    print(max(n1,n2,n3) *100)
